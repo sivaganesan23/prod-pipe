d3089a47c620c34a29a88f19180d6d99ad59053a
sivaganesan23 <45412181+sivaganesan23@users.noreply.github.com>
2019-03-22 21:36:03 +0530

    Update MAIN-prod-pipeline-job.groovy

--- jobs/pipeline-jobs/MAIN-prod-pipeline-job.groovy
+++ jobs/pipeline-jobs/MAIN-prod-pipeline-job.groovy
@@ -48,21 +48,7 @@ chmod 600 /home/centos/devops.pem
       sh 'rm -f /home/centos/devops.pem'
     }
 
-    try { 
-      dir('ANSIBLE') {
-         git 'https://github.com/sivaganesan23/prod-pipe.git'
-          withCredentials([file(credentialsId: 'CENTOS-USER-PEM-FILE', variable: 'FILE')]) {
-        sh '''
-        cat $FILE >/home/centos/devops.pem
-        chmod 600 /home/centos/devops.pem
-        ansible-playbook -i /tmp/hosts ansible_pull/deploy.yml
-        '''
-          }
-      }
-    } catch(Exception ex) {
-      sh 'rm -f /home/centos/devops.pem'
-      
-    } 
+
   }
 
         } 
